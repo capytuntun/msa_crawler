20af305a13224242e3301ae9df67c6eba4847179
alexchenys <yu.shiuan2017@gmail.com>
2025-03-02 13:45:24 +0800

    update nsa

--- main.py
+++ main.py
@@ -54,31 +54,29 @@ def nsa_crawler(nsa, url, today):
                 }
                 print(info)
 
-nsa_dict = {
-    '上海海事局': 'https://www.msa.gov.cn/page/openInfo/articleList.do?channelId=94DF14CE-1110-415D-A44E-67593E76619F&pageSize=20&pageNo=1&isParent=0',
-    '天津海事局': 'https://www.msa.gov.cn/page/openInfo/articleList.do?channelId=BDBA5FAD-6E5D-4867-9F97-0FCF8EFB8636&pageSize=20&pageNo=1&isParent=0',
-    '遼寧海事局': 'https://www.msa.gov.cn/page/openInfo/articleList.do?channelId=C8896863-B101-4C43-8705-536A03EB46FF&pageSize=20&pageNo=1&isParent=0',
-    '河北海事局': 'https://www.msa.gov.cn/page/openInfo/articleList.do?channelId=93B73989-D220-45F9-BC32-70A6EBA35180&pageSize=20&pageNo=1&isParent=0',
-    '山東海事局': 'https://www.msa.gov.cn/page/openInfo/articleList.do?channelId=36EA3354-C8F8-4953-ABA0-82D6D989C750&pageSize=20&pageNo=1&isParent=0',
-    '浙江海事局': 'https://www.msa.gov.cn/page/openInfo/articleList.do?channelId=8E10EA74-EB9E-4C96-90F8-F891968ADD80&pageSize=20&pageNo=1&isParent=0',
-    '福建海事局': 'https://www.msa.gov.cn/page/openInfo/articleList.do?channelId=7B084057-6038-4570-A0FB-44E9204C4B1D&pageSize=20&pageNo=1&isParent=0',
-    '廣東海事局': 'https://www.msa.gov.cn/page/openInfo/articleList.do?channelId=1E478D40-9E85-4918-BF12-478B8A19F4A8&pageSize=20&pageNo=1&isParent=0',
-    '廣西海事局': 'https://www.msa.gov.cn/page/openInfo/articleList.do?channelId=86DE2FFF-FF2C-47F9-8359-FD1F20D6508F&pageSize=20&pageNo=1&isParent=0',
-    '海南海事局': 'https://www.msa.gov.cn/page/openInfo/articleList.do?channelId=D3340711-057B-494B-8FA0-9EEDC4C5EAD9&pageSize=20&pageNo=1&isParent=0',
-    '長江海事局': 'https://www.msa.gov.cn/page/openInfo/articleList.do?channelId=93404234-06CC-4507-B2FB-8AF2492D2A3D&pageSize=20&pageNo=1&isParent=0',
-    '江蘇海事局': 'https://www.msa.gov.cn/page/openInfo/articleList.do?channelId=B5B0F3C7-630D-4967-B1E6-B06208575D15&pageSize=20&pageNo=1&isParent=0',
-    '深圳海事局': 'https://www.msa.gov.cn/page/openInfo/articleList.do?channelId=325FDC08-92B4-4313-A63E-E5C165BE98EC&pageSize=20&pageNo=1&isParent=0',
-    '連雲海事局': 'https://www.msa.gov.cn/page/openInfo/articleList.do?channelId=FA4501F3-DBE4-4F70-BC72-6F27132D4E04&pageSize=20&pageNo=1&isParent=0',
-    '江蘇省地方海事局': 'https://www.msa.gov.cn/page/openInfo/articleList.do?channelId=D14ED012-960B-4064-9712-70459A4A0D4D&pageSize=20&pageNo=1&isParent=0',
-    '江西省地方海事局': 'https://www.msa.gov.cn/page/openInfo/articleList.do?channelId=533B3954-E373-4C81-83E9-7D85B76BC9C5&pageSize=20&pageNo=1&isParent=0'
-}
+def main(nsa_dict):
+    today = datetime.today().strftime('%Y-%m-%d')
+    for key, value in nsa_dict.items():
+        nsa_crawler(key, value, today)
+        time.sleep(0.5)
 
-# today = datetime.today().strftime('%Y-%m-%d')
-today = '2025-02-28'
-for key, value in nsa_dict.items():
-    nsa_crawler(key, value, today)
-    time.sleep(0.5)
-# nsa_crawler('遼寧海事局', 'https://www.msa.gov.cn/page/openInfo/articleList.do?channelId=C8896863-B101-4C43-8705-536A03EB46FF&pageSize=20&pageNo=1&isParent=0')
-# news_crawler('https://www.msa.gov.cn/page/article.do?articleId=6201EE94-CF85-4EC5-83FE-41B2ACD04752&channelId=C8896863-B101-4C43-8705-536A03EB46FF')
-
-
+if __name__ == "__main__":
+    nsa_dict = {
+        '上海海事局': 'https://www.msa.gov.cn/page/openInfo/articleList.do?channelId=94DF14CE-1110-415D-A44E-67593E76619F&pageSize=20&pageNo=1&isParent=0',
+        '天津海事局': 'https://www.msa.gov.cn/page/openInfo/articleList.do?channelId=BDBA5FAD-6E5D-4867-9F97-0FCF8EFB8636&pageSize=20&pageNo=1&isParent=0',
+        '遼寧海事局': 'https://www.msa.gov.cn/page/openInfo/articleList.do?channelId=C8896863-B101-4C43-8705-536A03EB46FF&pageSize=20&pageNo=1&isParent=0',
+        '河北海事局': 'https://www.msa.gov.cn/page/openInfo/articleList.do?channelId=93B73989-D220-45F9-BC32-70A6EBA35180&pageSize=20&pageNo=1&isParent=0',
+        '山東海事局': 'https://www.msa.gov.cn/page/openInfo/articleList.do?channelId=36EA3354-C8F8-4953-ABA0-82D6D989C750&pageSize=20&pageNo=1&isParent=0',
+        '浙江海事局': 'https://www.msa.gov.cn/page/openInfo/articleList.do?channelId=8E10EA74-EB9E-4C96-90F8-F891968ADD80&pageSize=20&pageNo=1&isParent=0',
+        '福建海事局': 'https://www.msa.gov.cn/page/openInfo/articleList.do?channelId=7B084057-6038-4570-A0FB-44E9204C4B1D&pageSize=20&pageNo=1&isParent=0',
+        '廣東海事局': 'https://www.msa.gov.cn/page/openInfo/articleList.do?channelId=1E478D40-9E85-4918-BF12-478B8A19F4A8&pageSize=20&pageNo=1&isParent=0',
+        '廣西海事局': 'https://www.msa.gov.cn/page/openInfo/articleList.do?channelId=86DE2FFF-FF2C-47F9-8359-FD1F20D6508F&pageSize=20&pageNo=1&isParent=0',
+        '海南海事局': 'https://www.msa.gov.cn/page/openInfo/articleList.do?channelId=D3340711-057B-494B-8FA0-9EEDC4C5EAD9&pageSize=20&pageNo=1&isParent=0',
+        '長江海事局': 'https://www.msa.gov.cn/page/openInfo/articleList.do?channelId=93404234-06CC-4507-B2FB-8AF2492D2A3D&pageSize=20&pageNo=1&isParent=0',
+        '江蘇海事局': 'https://www.msa.gov.cn/page/openInfo/articleList.do?channelId=B5B0F3C7-630D-4967-B1E6-B06208575D15&pageSize=20&pageNo=1&isParent=0',
+        '深圳海事局': 'https://www.msa.gov.cn/page/openInfo/articleList.do?channelId=325FDC08-92B4-4313-A63E-E5C165BE98EC&pageSize=20&pageNo=1&isParent=0',
+        '連雲海事局': 'https://www.msa.gov.cn/page/openInfo/articleList.do?channelId=FA4501F3-DBE4-4F70-BC72-6F27132D4E04&pageSize=20&pageNo=1&isParent=0',
+        '江蘇省地方海事局': 'https://www.msa.gov.cn/page/openInfo/articleList.do?channelId=D14ED012-960B-4064-9712-70459A4A0D4D&pageSize=20&pageNo=1&isParent=0',
+        '江西省地方海事局': 'https://www.msa.gov.cn/page/openInfo/articleList.do?channelId=533B3954-E373-4C81-83E9-7D85B76BC9C5&pageSize=20&pageNo=1&isParent=0'
+    }
+    main(nsa_dict)
